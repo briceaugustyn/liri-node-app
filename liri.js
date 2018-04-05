b70db2ed13042b6e2635c052c50ea350354641a7
Brice Augustyn <baugustyn1000@gmail.com>
2018-04-05 13:21:17 -0700

    code 215 'Bad Authentification data'

--- liri.js
+++ liri.js
@@ -1,4 +1,24 @@
+
 require("dotenv").config;
 
-var spotify = new spotify(keys.spotify);
+//var spotify = new spotify(keys.spotify);
+let keys =  require('./keys.js');
+
+ 
+var Twitter = require('twitter');
 var client = new Twitter(keys.twitter);
+
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    console.log(error);  // Raw response object. 
+    if (!error) {
+    console.log(tweets); 
+  }
+});
